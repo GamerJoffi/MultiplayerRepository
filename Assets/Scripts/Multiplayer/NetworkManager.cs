d6200783ee76b1c551359ec965794a696fd8d2be
GamerJoffi <95049956+GamerJoffi@users.noreply.github.com>
2022-04-21 19:34:50 +0200

    Add files via upload

---
+++ Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using RiptideNetworking;
+using RiptideNetworking.Utils;
+using UnityEngine;
+
+public enum ServerToClientId : ushort
+{
+    playerSpawned = 1,
+}
+
+public enum CliendToServerId : ushort
+{
+    name = 1,
+}
+
+
+public class NetworkManager : MonoBehaviour
+{
+    private static NetworkManager _singleton;
+    public static NetworkManager Singelton
+    {
+        get => _singleton;
+        private set
+        {
+            if (_singleton == null)
+                _singleton = value;
+            else if (_singleton != value)
+            {
+                Debug.Log($"{nameof(NetworkManager)}Instance already exists, destroying duplicate!");
+                Destroy(value);
+            }
+        }
+    }
+    public Client Client { get; private set; }
+    [SerializeField] private string ip;
+    [SerializeField] private ushort port;
+    
+    private void Awake()
+    {
+        Singelton = this;
+    }
+
+    private void Start()
+    {
+        RiptideLogger.Initialize(Debug.Log, Debug.Log, Debug.LogWarning, Debug.LogError, false);
+
+        Client = new Client();
+        Client.Connected += DidConnect;
+        Client.ConnectionFailed += FaildToConnect;
+        Client.Disconnected += DidConnect;
+
+    }
+    
+    private void FixedUpdate()
+    {
+        Client.Tick();
+    }
+
+    private void OnApplicationQuit()
+    {
+        Client.Disconnect();
+    }
+
+    public void Connect()
+    {
+        Client.Connect($"{ip}:{port}");
+        
+    }
+
+    private void DidConnect(object sender, EventArgs e)
+    {
+        UIManager.Singelton.SendName();
+    }
+
+    private void FaildToConnect(object sender, EventArgs e)
+    {
+        UIManager.Singelton.BackToMain();
+    }
+
+    private void DidDisconnect(object sender, EventArgs e)
+    {
+        UIManager.Singelton.BackToMain();
+    }
+
+}
---
+++ Assets/Scripts/Multiplayer/UIManager.cs
@@ -0,0 +1,53 @@
+﻿using RiptideNetworking;
+using RiptideNetworking.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _singleton;
+    public static UIManager Singelton
+    {
+        get => _singleton;
+        private set
+        {
+            if (_singleton == null)
+                _singleton = value;
+            else if (_singleton != value)
+            {
+                Debug.Log($"{nameof(UIManager)}Instance already exists, destroying duplicate!");
+                Destroy(value);
+            }
+        }
+    }
+    [Header("Connect")]
+    [SerializeField] private GameObject connectUI;
+    [SerializeField] InputField usernameField;
+
+    private void Awake()
+    {
+        Singelton = this;
+    }
+
+    public void ConnectClicked()
+    {
+        usernameField.interactable = false;
+        connectUI.SetActive(false);
+
+        NetworkManager.Singelton.Connect();
+    }
+
+    public void BackToMain()
+    {
+        usernameField.interactable = true;
+        connectUI.SetActive(false);
+    }
+
+    public void SendName()
+    {
+        Message message = Message.Create(MessageSendMode.reliable, (ushort)CliendToServerId.name);
+    }
+}
